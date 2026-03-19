948fcaa25e7d9bcd8b98d5c756f69504deba6625
Ariella Duskis <aduskis@student.touro.edu>
2026-03-19 14:51:58 -0400

    start of the project, will fix model later

---
+++ src/main/java/org/example/SpirographModel.java
@@ -0,0 +1,49 @@
+package org.example;
+
+public class SpirographModel {
+    private double largeRadius;
+    private double smallRadius;
+    private double penDistance;
+    private double numSteps;
+    private double anglePerStep;
+
+    public double getNumSteps() {
+        return numSteps;
+    }
+
+    public void setNumSteps(double numSteps) {
+        this.numSteps = numSteps;
+    }
+
+    public double getLargeRadius() {
+        return largeRadius;
+    }
+
+    public void setLargeRadius(double largeRadius) {
+        this.largeRadius = largeRadius;
+    }
+
+    public double getSmallRadius() {
+        return smallRadius;
+    }
+
+    public void setSmallRadius(double smallRadius) {
+        this.smallRadius = smallRadius;
+    }
+
+    public double getPenDistance() {
+        return penDistance;
+    }
+
+    public void setPenDistance(double penDistance) {
+        this.penDistance = penDistance;
+    }
+
+    public double getAnglePerStep() {
+        return anglePerStep;
+    }
+
+    public void setAnglePerStep(double anglePerStep) {
+        this.anglePerStep = anglePerStep;
+    }
+}
---
+++ src/main/java/org/example/SpirographView.java
@@ -0,0 +1,7 @@
+package org.example;
+
+import javax.swing.*;
+
+public class SpirographView extends JComponent {
+    //SpirographModel draw = new SpirographModel(1,1,1,1,1 );
+}
